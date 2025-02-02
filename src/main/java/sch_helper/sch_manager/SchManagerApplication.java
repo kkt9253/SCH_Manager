39e52edc4068c2ad3ecb92d3161f2d88985266fa
kkt9253 <kkt9253@naver.com>
2025-02-02 12:53:52 +0900

    DOCS : 초기 설정

---
+++ src/main/java/sch_helper/sch_manager/SchManagerApplication.java
@@ -0,0 +1,13 @@
+package sch_helper.sch_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sch_helper/sch_manager/SchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package sch_helper.sch_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
