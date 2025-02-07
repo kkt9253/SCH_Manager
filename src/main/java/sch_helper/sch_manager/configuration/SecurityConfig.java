cb5cbb06ed01418d257249d6d023c83d44fb11ba
kkt9253 <kkt9253@naver.com>
2025-02-08 01:23:20 +0900

    FEAT : Spring Security 설정 추가 및 변경
    - "/reissue" 엔드포인트 허용 추가
    - JwtFilter가 LoginFilter 앞단에 위치하여 발생했던 무한 로그인 문제 해결
    - RefreshTokenHelper 추가
    - CustomLogoutFilter 등록 및 기본 Security Logout 필터 비활성화

--- src/main/java/sch_helper/sch_manager/configuration/SecurityConfig.java
+++ src/main/java/sch_helper/sch_manager/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package sch_helper.sch_manager.configuration;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,19 +12,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import sch_helper.sch_manager.auth.CookieUtil;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import sch_helper.sch_manager.auth.CustomLogoutFilter;
+import sch_helper.sch_manager.auth.util.CookieUtil;
 import sch_helper.sch_manager.auth.JwtFilter;
-import sch_helper.sch_manager.auth.JwtUtil;
+import sch_helper.sch_manager.auth.util.JwtUtil;
 import sch_helper.sch_manager.auth.LoginFilter;
+import sch_helper.sch_manager.auth.util.RefreshTokenHelper;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtUtil jwtUtil;
     private final CookieUtil cookieUtil;
+    private final RefreshTokenHelper refreshTokenHelper;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -43,19 +48,24 @@ public class SecurityConfig {
         http.csrf(auth -> auth
                 .disable());
 
-        http.formLogin(auth -> auth
-                .disable());
-
         http.httpBasic(auth -> auth
                 .disable());
 
+        http.logout(auth -> auth
+                .disable());
+
+        http.formLogin(auth -> auth
+                .disable());
+
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers( "/login").permitAll()
+                .requestMatchers( "/login", "/reissue").permitAll()
                 .anyRequest().authenticated());
 
-        http.addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+        http.addFilterAt(new CustomLogoutFilter(refreshTokenHelper, cookieUtil), LogoutFilter.class);
 
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, cookieUtil), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, cookieUtil, refreshTokenHelper), UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterAfter(new JwtFilter(jwtUtil), LoginFilter.class);
 
         http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
