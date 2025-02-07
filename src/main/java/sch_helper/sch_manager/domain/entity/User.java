a6923748d13875b9474e38c7404faa3bcf75aac4
kkt9253 <kkt9253@naver.com>
2025-02-08 00:50:12 +0900

    REFACTOR : User 엔티티 필드 및 어노테이션 개선

--- src/main/java/sch_helper/sch_manager/domain/entity/User.java
+++ src/main/java/sch_helper/sch_manager/domain/entity/User.java
@@ -2,21 +2,27 @@ package sch_helper.sch_manager.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import sch_helper.sch_manager.domain.Role;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 }
