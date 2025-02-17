f3c8585a3f6e2eff9167fbebb3a53d29c97deaea
kkt9253 <kkt9253@naver.com>
2025-02-17 23:26:10 +0900

    FIX : reissue 요청 시 예외처리 버그 수정
    - Spring container에서 처리되어야 하는 놓친 예외를 GlobalExceptionHandler에서 처리하도록 해결

--- src/main/java/sch_helper/sch_manager/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/sch_helper/sch_manager/common/exception/handler/GlobalExceptionHandler.java
@@ -2,22 +2,33 @@ package sch_helper.sch_manager.common.exception.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sch_helper.sch_manager.common.exception.custom.JwtAuthenticationException;
 import sch_helper.sch_manager.common.exception.error.ErrorCode;
 import sch_helper.sch_manager.common.response.ErrorResponse;
+
+import java.io.IOException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(HttpServletRequest request, Exception ex) {
+    @ExceptionHandler(JwtAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleJwtAuthenticationException(JwtAuthenticationException ex) {
 
-        log.error("Unhandled exception => {}", ex.getMessage());
+        log.error("JwtAuthentication Exception: {}", ex.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.of(ex.getErrorCode(), ex.getErrorCode().getMessage());
+        return ResponseEntity.status(ex.getErrorCode().getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(HttpServletRequest request, Exception ex) {
+        log.error("Unhandled exception: {}", ex.getMessage());
         ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, request.getRequestURI());
-        return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus()).body(errorResponse);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
 }
