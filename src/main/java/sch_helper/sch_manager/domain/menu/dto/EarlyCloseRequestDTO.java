a401bfbcb44a2eb438d7273dfa72096dedd78027
Seong57 <gustjr7297@naver.com>
2025-04-01 13:41:43 +0900

    FIX : 래퍼타입으로 변경

--- src/main/java/sch_helper/sch_manager/domain/menu/dto/EarlyCloseRequestDTO.java
+++ src/main/java/sch_helper/sch_manager/domain/menu/dto/EarlyCloseRequestDTO.java
@@ -1,17 +1,13 @@
 package sch_helper.sch_manager.domain.menu.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class EarlyCloseRequestDTO {
 
-    // mealType 은 필요 없으므로 넣지 않음
-
-    @NotBlank
-    private boolean earlyClose;
+    @NotNull(message = "earlyClose는 필수값입니다.")
+    private Boolean earlyClose;
 }
