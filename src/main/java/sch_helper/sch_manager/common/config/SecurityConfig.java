89e8f82f62ece699eccab0cc9a18c19715386bfd
Seong57 <gustjr7297@naver.com>
2025-04-02 18:59:05 +0900

    FEAT: 권한별 접근 제어, cors 허용

--- src/main/java/sch_helper/sch_manager/common/config/SecurityConfig.java
+++ src/main/java/sch_helper/sch_manager/common/config/SecurityConfig.java
@@ -9,11 +9,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sch_helper.sch_manager.auth.filter.CustomLogoutFilter;
 import sch_helper.sch_manager.auth.filter.JwtExceptionFilter;
 import sch_helper.sch_manager.auth.security.CustomAccessDeniedHandler;
@@ -24,6 +28,9 @@ import sch_helper.sch_manager.auth.util.JwtUtil;
 import sch_helper.sch_manager.auth.filter.LoginFilter;
 import sch_helper.sch_manager.auth.util.RefreshTokenHelper;
 import sch_helper.sch_manager.domain.user.enums.Role;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -52,37 +59,37 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
 
-        http.csrf(auth -> auth
-                .disable());
+        http
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(HttpMethod.POST, "/api/admin/week-meal-plans/HYANGSEOL1/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/week-meal-plans/FACULTY/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/meal-plans/HYANGSEOL1/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/meal-plans/FACULTY/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/early-close/HYANGSEOL1/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/early-close/FACULTY/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/total-operating-time/HYANGSEOL1/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
+                                .requestMatchers(HttpMethod.POST, "/api/admin/total-operating-time/FACULTY/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
+                                .requestMatchers(HttpMethod.GET, "/api/admin/**").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name(), Role.Admin3.name())
+                                .requestMatchers(HttpMethod.POST, "/login", "/reissue").permitAll()
+                                .requestMatchers("/api/user/**").permitAll()
+                                .requestMatchers("/api/master/**").hasAnyAuthority(Role.Master.name())
+                                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                );
 
-        http.httpBasic(auth -> auth
-                .disable());
-
-        http.logout(auth -> auth
-                .disable());
-
-        http.formLogin(auth -> auth
-                .disable());
-
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers( HttpMethod.POST, "/login", "/reissue").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/admin/week-meal-plans/hyangseol1").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
-                .requestMatchers(HttpMethod.POST, "/api/admin/week-meal-plans/faculty").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
-                .requestMatchers(HttpMethod.POST, "/api/admin/meal-plans/hyangseol1").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin2.name())
-                .requestMatchers(HttpMethod.POST, "/api/admin/meal-plans/faculty").hasAnyAuthority(Role.Master.name(), Role.Admin1.name(), Role.Admin3.name())
-                .requestMatchers(HttpMethod.POST, "/api/master/meal-plans/hyangseol1").hasAnyAuthority(Role.Master.name())
-                .requestMatchers(HttpMethod.POST, "/api/master/meal-plans/faculty").hasAnyAuthority(Role.Master.name())
-                // 추가된 GET - api 설정해야 함 (근데 설정하기 애매해서 고민 중)
-                .anyRequest().authenticated());
-
-        http.addFilterBefore(new JwtExceptionFilter(objectMapper), LogoutFilter.class);
-
-        http.addFilterAt(new CustomLogoutFilter(refreshTokenHelper, cookieUtil, jwtUtil, objectMapper), LogoutFilter.class);
-
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, cookieUtil, refreshTokenHelper, objectMapper), UsernamePasswordAuthenticationFilter.class);
-
-        http.addFilterAfter(new JwtFilter(jwtUtil), LoginFilter.class);
+        http
+                .addFilterBefore(new JwtExceptionFilter(objectMapper), LogoutFilter.class)
+                .addFilterAfter(new JwtFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new CustomLogoutFilter(refreshTokenHelper, cookieUtil, jwtUtil, objectMapper), LogoutFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, cookieUtil, refreshTokenHelper, objectMapper), UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling(e -> e
                 .authenticationEntryPoint(authenticationEntryPoint)
@@ -92,5 +99,17 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
